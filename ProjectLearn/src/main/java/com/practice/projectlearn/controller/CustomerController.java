b6d697fedbfa432d3816e46be443722de75cf1ba
Sagar Chauhan <schauhan@miu.edu>
2024-10-24 14:50:13 -0400

    new project created

---
+++ ProjectLearn/src/main/java/com/practice/projectlearn/controller/CustomerController.java
@@ -0,0 +1,4 @@
+package com.practice.projectlearn.controller;
+
+public class CustomerController {
+}
---
+++ ProjectLearn/src/test/java/com/practice/projectlearn/ProjectLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.projectlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
